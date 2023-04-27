570fb59148ee6650c4c267d3d888deb37c4b4860
Artur <petrikartur@gmail.com>
2023-04-27 17:09:25 +0300

    fix algorithm, add missing permutations

--- backend/apps/rephrase/utils.py
+++ backend/apps/rephrase/utils.py
@@ -11,7 +11,7 @@ def make_ptree(s: str) -> ParentedTree:
 
 
 def rephrased_ptree_generator(
-    ptree: ParentedTree, initial_tree: ParentedTree
+    ptree: ParentedTree, initial_ptree: ParentedTree, start_from: tuple[int] = ()
 ) -> Generator[ParentedTree, None, None]:
     # stop when reach the leaf value
     try:
@@ -19,20 +19,23 @@ def rephrased_ptree_generator(
     except AttributeError:
         return
 
-    if ptree.label() == NP:
+    cur_pos = ptree.treeposition()
+    if ptree.label() == NP and cur_pos > start_from:
         inner_np_trees = {
             subtree.treeposition(): subtree for subtree in ptree if subtree.label() == NP
         }
         if len(inner_np_trees) > 1:
             for perm in list(permutations(inner_np_trees.values()))[1:]:  # get only unique perms
-                new_tree = initial_tree.copy(deep=True)
+                new_tree = initial_ptree.copy(deep=True)
                 for idx, key in enumerate(inner_np_trees):
                     # to be able to set a new subtree: copy subtree to get rid of `parent` param
                     new_tree[key] = perm[idx].copy(deep=True)
                 yield new_tree
+                # also paraphrase trees from `new_tree` if there are some NPs left after current pos
+                yield from rephrased_ptree_generator(new_tree, new_tree, cur_pos)
 
     for node in ptree:
-        yield from rephrased_ptree_generator(node, initial_tree)
+        yield from rephrased_ptree_generator(node, initial_ptree, start_from)
 
 
 def generate_rephrased_ptrees(ptree: ParentedTree, limit: int = 20) -> list[ParentedTree]:
